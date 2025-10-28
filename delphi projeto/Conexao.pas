d8c38ac4715f07df4df666ade3ae18ac6bbf31f1
Emili Caroline de Olveira <emili.caroline04@gmail.com>
2025-10-27 23:27:56 -0300

    no message

--- delphi projeto/Conexao.pas
+++ delphi projeto/Conexao.pas
@@ -16,6 +16,10 @@ type
     FDPhysFBDriverLink1: TFDPhysFBDriverLink;
     DataSource1: TDataSource;
     FDQuery1: TFDQuery;
+    FDQueryConta: TFDQuery;
+    DataSource2: TDataSource;
+    FDQueryServicos: TFDQuery;
+    DataSourceServicos: TDataSource;
   private
     { Private declarations }
   public
